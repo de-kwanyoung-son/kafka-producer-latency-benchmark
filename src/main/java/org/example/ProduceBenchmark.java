8b3c033a1f9f4d3c68a7a8c5446a1602353c571b
kwanyoung-ably <100560062+kwanyoung-ably@users.noreply.github.com>
2022-08-02 19:24:15 +0900

    add kafka producer latency benchmark

---
+++ src/main/java/org/example/ProduceBenchmark.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+public class ProduceBenchmark {
+
+    private Producer<String, String> producer;
+    private int key = 1;
+    private final String value = "Benchmark";
+
+    @Setup
+    public void setup() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", System.getenv("BOOTSTRAP_SERVERS"));
+        props.put("acks", "1");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        producer = new KafkaProducer<>(props);
+    }
+
+    @TearDown
+    public void teardown() {
+        producer.close();
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.SampleTime})
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureUnpartitioned() {
+        producer.send(new ProducerRecord<>("benchmark_latency_unpartitioned", Integer.toString(key), value));
+        key++;
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.SampleTime})
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureTwoPartitioned() {
+        producer.send(new ProducerRecord<>("benchmark_latency_partitioned", Integer.toString(key), value));
+        key++;
+    }
+
+    @Benchmark
+    @BenchmarkMode({Mode.AverageTime, Mode.SampleTime})
+    @OutputTimeUnit(TimeUnit.MICROSECONDS)
+    public void measureTenPartitioned() {
+        producer.send(new ProducerRecord<>("benchmark_latency_partitioned_ten", Integer.toString(key), value));
+        key++;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(ProduceBenchmark.class.getSimpleName())
+                .forks(1)
+                .build();
+
+        new Runner(opt).run();
+    }
+}
